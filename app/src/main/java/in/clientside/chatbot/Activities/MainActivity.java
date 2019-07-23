2ff58047c04cefd47ca25279836b7a66dc03c0ff
Zaid <monis.z.786@gmail.com>
2019-07-23 23:57:03 +0530

    implemented text input.

--- app/src/main/java/in/clientside/chatbot/Activities/MainActivity.java
+++ app/src/main/java/in/clientside/chatbot/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package in.clientside.chatbot.Activities;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
@@ -14,8 +15,12 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,9 +33,11 @@ import java.util.Locale;
 import java.util.Map;
 
 import ai.api.AIListener;
+import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
 import ai.api.model.AIError;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
 import in.clientside.chatbot.R;
@@ -46,14 +53,15 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
     boolean flag;
     Result result;
     Button listenButton;
+    private EditText editTextMessage;
     private RecyclerView recyclerView;
     private TextView resultTextView, messageText, queryText;
     private AIService aiService;
+    private AIRequest aiRequest;
     private ChatAdapter mAdapter;
     private final int REQ_CODE_SPEECH_INPUT = 100;
     private String customerID;
     private TextToSpeech textToSpeech;
-    int recyclerScrollY;
     RecyclerView.LayoutManager mLayoutManager;
 
 
@@ -66,13 +74,29 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
                 new String[]{Manifest.permission.RECORD_AUDIO},
                 RECORD_REQUEST_CODE);
     }
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        public void afterTextChanged(Editable s) {
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!editTextMessage.getText().toString().equals("")){
+                listenButton.setText("Send");
+            }
+            else{
+                listenButton.setText("Listen");
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.getSupportActionBar().hide();
-
         textToSpeech = new TextToSpeech(this, this);
         textToSpeech.setLanguage(Locale.ENGLISH);
 
@@ -85,8 +109,10 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
         flag = false;
 
         listenButton = findViewById(R.id.listenButton);
+        editTextMessage = findViewById(R.id.editText);
         recyclerView = findViewById(R.id.recycler_view);
         initializeActionList();
+        editTextMessage.addTextChangedListener(textWatcher);
 
         mAdapter = new ChatAdapter(chatList);
         mLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -94,20 +120,68 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
-        final AIConfiguration config = new AIConfiguration("5ccf99fb6c5d4af2b20f5aeb583651e2  ",
+        final AIConfiguration config = new AIConfiguration("c1a8566f0e81483aaf5c51e9d8eeede4",
                 AIConfiguration.SupportedLanguages.DEFAULT,
                 AIConfiguration.RecognitionEngine.System);
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
+        aiRequest = new AIRequest();
+
 
 
 
         listenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                listenButtonOnClick();
+                if (listenButton.getText().equals("Listen")){
+                    aiService.startListening();
+                }
+                else{
+                    aiRequest.setQuery(editTextMessage.getText().toString());
+                    Chat chat = new Chat(editTextMessage.getText().toString(), "me");
+                    editTextMessage.setText("");
+                    chatList.add(chat);
+                    changeRecyclerView();
+                    SendTextRequest sendTextRequest = new SendTextRequest();
+                    sendTextRequest.execute(aiRequest);
+                }
             }
         });
+    }
+
+    private class SendTextRequest extends AsyncTask<AIRequest, Void, AIResponse>{
+
+        @Override
+        protected AIResponse doInBackground(AIRequest... aiRequests) {
+            final AIRequest aiRequest = aiRequests[0];
+            AIResponse response;
+            try {
+                response = aiService.textRequest(aiRequest);
+                return response;
+            } catch (AIServiceException e) {
+                System.out.println(""+e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(AIResponse aiResponse) {
+            super.onPostExecute(aiResponse);
+            MainActivity.this.callback(aiResponse);
+        }
+    }
+    public void callback(AIResponse aiResponse){
+        Toast.makeText(this, "here", Toast.LENGTH_SHORT).show();
+        if (aiResponse!=null){
+            String receivedMessage = aiResponse.getResult().getFulfillment().getSpeech();
+            Chat chat = new Chat(receivedMessage, "him");
+            chatList.add(chat);
+            textToSpeech.speak(receivedMessage, TextToSpeech.QUEUE_FLUSH, null);
+            changeRecyclerView();
+        }
+        else{
+            Toast.makeText(this, "empty", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void initializeActionList() {
@@ -230,5 +304,18 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
             }
 
         }
+    }
+    @Override
+    protected void onDestroy() {
+
+
+        //Close the Text to Speech Library
+        if(textToSpeech != null) {
+
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+            Log.d(TAG, "TTS Destroyed");
+        }
+        super.onDestroy();
     }
 }
